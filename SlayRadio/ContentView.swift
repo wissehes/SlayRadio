459319a297fac0142e9c2b45427df9612e29684c
Wisse <wissehes@gmail.com>
2022-07-24 17:40:51 +0200

    Initial Commit

---
+++ SlayRadio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlayRadio
+//
+//  Created by Wisse Hes on 24/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlayRadio/SlayRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlayRadioApp.swift
+//  SlayRadio
+//
+//  Created by Wisse Hes on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SlayRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
